45424a2ce61cc0971b4e2e3c6a3d5f7c7d6d7ac4
fame <famephp@gmail.com>
2022-11-11 11:08:09 +0500

    05

--- find05_max_even.py
+++ find05_max_even.py
@@ -7,7 +7,8 @@ def find_max_even(data):
     """
     max = data[0]
     for i in data:
-        if i > max and i%2 == 0:
-            max = i
+        if i > max:
+            if i%2 == 0:
+                max = i
 
     return max
