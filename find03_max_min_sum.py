6f7cbae7661ef2aa8fa3ba75959b82553fea8c45
fame <famephp@gmail.com>
2022-11-11 10:53:29 +0500

    03

--- find03_max_min_sum.py
+++ find03_max_min_sum.py
@@ -5,4 +5,15 @@ def find_max_min_sum(data):
         data: list of numbers
     returns: sum of the maximum and minimum numbers in the list
     """
-    return 0
+    max = data[0]
+    min = data[0]
+
+    for i in data:
+        if i > max:
+            max = i
+
+        elif i < min:
+            min =i
+
+    return max + min
+
