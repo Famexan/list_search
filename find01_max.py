daf7a076e9678e5189b72ad33d5a8487a87e8d45
fame <famephp@gmail.com>
2022-11-11 10:31:09 +0500

    01

--- find01_max.py
+++ find01_max.py
@@ -1,9 +1,15 @@
 def find_max(data):
+    """
+    Given the list of numbers, return the maximum number in the list
+    args:
+        data: list of numbers
+    returns: maximum number in the list
+    """
+
     max = data[0]
     for i in data:
         if i > max:
             max = i
-
 
     return max
     
